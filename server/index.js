596ee613ccf2738c00e1cd7a97f69050f1e1c459
Alexandr T <vip-tai@ya.ru>
2021-08-24 23:55:29 +0600

    fix kjsdffds

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.get("(/room/*)", (req, res) => {
+app.get("/room/*", (req, res) => {
   // res.set({ 'Content-Security-Policy': 'frame-ancestors https://77.223.96.62 http://77.223.96.62 https://www.linkchat.io https://linkchat.io https://www.online.enggo.kz/ https://online.enggo.kz/' })
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
