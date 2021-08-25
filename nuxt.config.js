2237a29481c8aa48c5c47933ae1b6455a6a71ca6
Alexandr T <vip-tai@ya.ru>
2021-08-25 21:43:46 +0600

    fix asd

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
   },
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
-    baseURL: `${process.env.NODE_ENV === 'production' ? 'https://' + process.env.HOST + ':3443/api/v1' : 'https://localhost:3443/api/v1'}`
+    baseURL: `${process.env.NODE_ENV === 'production' ? 'https://' + process.env.HOST + ':3001/api/v1' : 'https://localhost:3001/api/v1'}`
   },
 
   telemetry: false,
