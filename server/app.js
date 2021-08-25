b614b171654609b1f31fdf097ae3d7fa5df905fe
Alexandr T <vip-tai@ya.ru>
2021-08-25 22:50:14 +0600

    asd nklasd

--- server/app.js
+++ server/app.js
@@ -20,9 +20,9 @@ const corsOptionsDelegate = function (req, callback) {
     optionsSuccessStatus: 204
   }
   if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions.origin = true // reflect (enable) the requested origin in the CORS response
+    corsOptions.origin = false // reflect (enable) the requested origin in the CORS response
   } else {
-    corsOptions.origin = false // disable CORS for this request
+    corsOptions.origin = true // disable CORS for this request
   }
   callback(null, corsOptions) // callback expects two parameters: error and options
 }
