ed35c2d2d95dd66c67c2231d29aa76883b88df4e
Alexandr T <vip-tai@ya.ru>
2021-08-26 11:50:08 +0600

    add cors

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ const app = express()
 //   callback(null, corsOptions) // callback expects two parameters: error and options
 // }
 
-// app.use(cors(corsOptionsDelegate))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
